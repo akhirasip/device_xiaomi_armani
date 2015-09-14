b23f8ae9aa316e7425502f16132a2cb16bf9e63b
Anonim47 <yosef@yosefadi.tk>
2015-09-14 04:07:14 -0400

    Update for Xoplax OS
    Signed-off-by: Anonim47 <yosef@yosefadi.tk>

--- xoplax.mk
+++ xoplax.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/xiaomi/armani/device.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xoplax/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := armani
@@ -30,6 +30,7 @@ PRODUCT_NAME := cm_armani
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := HM 1S
 PRODUCT_MANUFACTURER := Xiaomi
+XOPLAX_TARGET := HM1S
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
